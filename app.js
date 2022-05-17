8eaed8bbc2ce314922b881664929a799ae9f2e5e
mpereah <martin.perea.hernandez@gmail.com>
2022-05-16 19:06:47 -0500

    se corrigio la app.js, los archivos package y lock porque marcaba error al ejecutar la insersion de nueva card porque marcaba 3 vulnerabilidades y e instale npm audit fix --force y eso causo error y se reinstalo de nuevo trello

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 require('dotenv').config()
 
-if (process.env.TOKEN && process.env.KEY){
+if (!process.env.TOKEN && !process.env.KEY){
     throw new Error('no hay configuracion de API Key y Token')
 }
+
+let Trello = require("trello");
+let trello = new Trello(process.env.KEY, process.env.TOKEN);
+
+let carTitle = `Card nueva ${new Date()}`
+
+trello.addCard(carTitle, "LaunchX Card Description", "627ec3d9c7e23b105359fba8",
+    function (error, trelloCard){
+        if (error){
+            console.log('Could not add Card', error);
+    }   
+        else{
+            console.log('Added card:', trelloCard);
+    }
+})
